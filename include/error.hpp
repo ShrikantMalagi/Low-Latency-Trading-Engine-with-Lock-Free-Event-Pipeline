b5af79539eee3b9b1e57a6b567e7b5108e5cb336
ShrikantMalagi <shrikant.malagi08@gmail.com>
2026-03-01 15:46:58 -0800

    yes

--- include/error.hpp
+++ include/error.hpp
@@ -4,28 +4,30 @@
 #include <cerrno>
 #include <cstring>
 
-enum class ErrorCode {
-    SocketCreate,
-    SetSockOpt,
-    Bind,
-    Listen,
-    Accept,
-    Read,
-    Write,
-    PeerClosed,
-    InvalidMessage,
-};
-
-struct Error {
-    ErrorCode code;
-    int sys_errno = 0;
-    std::string_view context;
-};
-
-inline Error make_sys_error(ErrorCode code, std::string_view context) {
-    return Error{
-        .code = code,
-        .sys_errno = errno,
-        .context = context
+namespace hft {
+    enum class ErrorCode {
+        SocketCreate,
+        SetSockOpt,
+        Bind,
+        Listen,
+        Accept,
+        Read,
+        Write,
+        PeerClosed,
+        InvalidMessage,
     };
+
+    struct Error {
+        ErrorCode code;
+        int sys_errno = 0;
+        std::string_view context;
+    };
+
+    inline Error make_sys_error(ErrorCode code, std::string_view context) {
+        return Error{
+            .code = code,
+            .sys_errno = errno,
+            .context = context
+        };
+    }
 }
